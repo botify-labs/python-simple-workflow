b60fd6e25a23523b19fb90450ad969c6813c82e1
Theo Crevon <tcrevon@gmail.com>
2013-03-27 12:04:20 +0100

    Update: Document Domain object

--- swf/domain.py
+++ swf/domain.py
@@ -6,6 +6,22 @@ from .utils import requires_connexion
 
 
 class Domain(object):
+    """Simple Workflow Domain wrapper
+
+    Params
+    ------
+    * name:
+        * type: String
+        * value: Name of the domain to register (unique)
+
+    * retention_period
+        * type: Integer
+        * value: Domain's workflow executions records retention in days
+
+    * description
+        * type: String
+        * value: Textual description of the domain
+    """
     def __init__(self, name, retention_period,
                  description=None, *args, **kwargs):
         self._connexion = kwargs.pop('connexion', None)
@@ -26,16 +42,19 @@ class Domain(object):
     @property
     @requires_connexion
     def exists(self):
+        """Checks if the domain exists amazon-side"""
         domains = self.connexion.layer.list_domains(DOMAIN_REGISTERED)['domainInfos']
 
         return any(d['name'] == self.name for d in domains)
 
     @requires_connexion
     def save(self):
+        """Creates the domain amazon side"""
         self.connexion.layer.register_domain(self.name,
                                              self.retention_period,
                                              self.description)
 
     @requires_connexion
     def deprecate(self):
+        """Deprecates the domain amazon side"""
         self.connexion.layer.deprecate_domain(self.name)
--- swf/utils.py
+++ swf/utils.py
@@ -8,6 +8,15 @@ class CredentialsException(Exception):
 
 
 def requires_connexion(fn):
+    """Ensures decorated method obect's exposes a valid connexion object
+
+    Any object pretending to talk with amazon simple workflow webservice
+    should expose a valid `connexion` attribute. See swf.base.Connexion.
+
+    This decorator will also ensure that credentials are valid, and
+    will raise a CredentialsException with a plain error message when
+    it's not the case.
+    """
     def wrapped(self):
         if not hasattr(self, '_connexion') or not self.connexion:
             err_msg = "Missing AWS credentials, please set connexion "\
