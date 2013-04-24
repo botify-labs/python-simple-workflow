2dcf4763ac9ff29d5f859d6cbd74ad8c79c64445
Theo Crevon <tcrevon@gmail.com>
2013-04-24 10:18:03 +0200

    Fix: remove models legacy import

--- swf/models/__init__.py
+++ swf/models/__init__.py
@@ -9,6 +9,4 @@ from swf.models.base import BaseModel
 from swf.models.activity import ActivityType, ActivityTask
 from swf.models.domain import Domain
 from swf.models.workflow import WorkflowType, WorkflowExecution
-from swf.models.decider import Decider
-from swf.models.task import Task, TaskList
 from swf.models.event import Event, History
