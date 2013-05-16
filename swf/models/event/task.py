990241adf820c8705ae3eb8637be090d736a93f4
Theo Crevon <tcrevon@gmail.com>
2013-05-16 15:16:41 +0200

    Add: compiled tasks events

--- swf/models/event/task.py
+++ swf/models/event/task.py
@@ -6,11 +6,57 @@
 # See the file LICENSE for copying permission.
 
 from swf.models.event.base import Event
+from swf.models.event.compiler import CompiledEvent
 
 
 class ActivityTaskEvent(Event):
     _type = 'ActivityTask'
 
 
+class CompiledActivityTaskEvent(CompiledEvent):
+    _type = 'ActivityTask'
+    states = (
+        'scheduled',
+        'schedule_failed',
+        'started',
+        'completed',
+        'failed',
+        'timed_out',
+        'canceled',
+        'cancel_requested',
+        'request_cancel_failed',
+    )
+
+    transitions = {
+        'scheduled': ('schedule_failed', 'canceled', 'timed_out', 'started'),
+        'schedule_failed': ('scheduled', 'timed_out'),
+        'started': ('canceled', 'failed', 'timed_out', 'completed'),
+        'failed': ('scheduled', 'timed_out'),
+        'timed_out': ('scheduled'),
+        'canceled': ('scheduled', 'timed_out'),
+        'cancel_requested': ('canceled', 'request_cancel_failed', 'timed_out'),
+        'request_cancel_failed': ('scheduled', 'timed_out'),
+    }
+
+    initial_state = 'scheduled'
+
 class DecisionTaskEvent(Event):
     _type = 'DecisionTask'
+
+
+class CompiledDecisionTaskEvent(CompiledEvent):
+    _type = 'DecisionTask'
+    states = (
+        'scheduled',
+        'started',
+        'completed',
+        'timed_out'
+    )
+
+    transitions = {
+        'scheduled': ('started'),
+        'started': ('timed_out', 'completed'),
+        'timed_out': ('scheduled'),
+    }
+
+    initial_state = 'scheduled'
