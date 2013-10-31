87de1fce846d7f50017fba885725a0907d43275e
Oleiade <tcrevon@gmail.com>
2013-10-31 16:20:43 +0100

    Add history qs to swf querysets modules

--- swf/querysets/__init__.py
+++ swf/querysets/__init__.py
@@ -2,5 +2,6 @@
 
 from swf.querysets.activity import ActivityTypeQuerySet
 from swf.querysets.domain import DomainQuerySet
+from swf.querysets.history import HistoryQuerySet
 from swf.querysets.workflow import (WorkflowTypeQuerySet,
                                     WorkflowExecutionQuerySet)
